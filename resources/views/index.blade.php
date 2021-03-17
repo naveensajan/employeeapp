627a861a1b912be5ef689689a1f0d3fe473a5bc6
naveensajan <naveensajan@gmail.com>
2021-03-17 14:34:17 +0530

    emp

---
+++ resources/views/index.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active " aria-current="page" href="/">Add Employee</a>
+        <a class="nav-link " href="/search">Search Employee</a>
+    
+      </div>
+    </div>
+  </div>
+</nav>
+  <div class="container">
+  <div class="row">
+  <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+  <table class="table table-borderless">
+<center><h1><b>Add Employee</b></h1></center>
+  <tr>
+      <td>Emp name</td>
+      <td><input type="text" class="form-control"></td>
+  </tr>
+  <tr>
+      <td>Emp code</td>
+      <td><input type="text" class="form-control"></td>
+  </tr>
+  <tr>
+      <td>designation</td>
+      <td><input type="text" class="form-control"></td>
+  </tr>
+  <tr>
+      <td>Salary</td>
+      <td><input type="text" class="form-control"></td>
+  </tr>
+  <tr>
+      <td>DOB</td>
+      <td><date class="form-control"></date></td>
+  </tr>
+  <tr>
+      <td>Place</td>
+      <td><input type="text" class="form-control"></td>
+  </tr>
+  <tr>
+      <td>Pincode</td>
+      <td><input type="text" class="form-control"></td>
+  </tr>
+  <tr>
+      <td>Phone no</td>
+      <td><input type="text" class="form-control"></td>
+  </tr>
+  <tr>
+      <td></td>
+      <td><button class="btn btn-secondary">Sumbit</button></td>
+  </tr>
+    
+  </table>
+  
+  </div>
+  </div>
+  </div>
+    
+</body>
+</html>
